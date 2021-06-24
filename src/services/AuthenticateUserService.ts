cd8f264c15043db310b1d71233e4e34b4c70a539
Alexandre Sobota-Linux <alexandre.sobota@hotmail.com>
2021-06-24 14:02:51 -0300

    Fourth class

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,48 @@
+import { getCustomRepository } from "typeorm";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import { UserRepositories } from "../repositories/UserRepositories";
+
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+
+  async execute({email, password}: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UserRepositories);
+
+    // Verificar se email existe
+    const user = await usersRepositories.findOne({
+      email
+    });
+
+    if(!user) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    // Verificar se a senha está correta
+    const passwordMatch = await compare(password, user.password);
+
+    if(!passwordMatch) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    // Gerar token
+    const token = sign(
+      {
+        email: user.email
+      }, "c556c507dfa28741c903b1d6ff7f05fa", {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService }
